03ef2d82b8aa098bdad13ec8b723210bd6a175cb
xzchaoo <70862045@qq.com>
2015-12-08 22:59:57 +0800

    2259

--- test-bilibili/src/main/java/org/xzc/bilibili/CommentService.java
+++ test-bilibili/src/main/java/org/xzc/bilibili/CommentService.java
@@ -12,7 +12,7 @@ public class CommentService {
 	public boolean accept(Video v) {
 		if (v.mid == 1643718) //up主是山下智博
 			return true;
-		return v.typeid == 33 || v.typeid == 31 || v.typeid == 20 || v.typeid == 32 || v.typeid == 15;
+		return v.typeid == 33/* || v.typeid == 31 || v.typeid == 20*/ || v.typeid == 32/* || v.typeid == 15*/;
 	}
 
 	private Map<Integer, String> 预定的文本 = new HashMap<Integer, String>();
