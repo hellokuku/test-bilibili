07c58025f4686546f300d863a828c0465477863c
17177 <17177@abc.cn>
2015-12-09 15:01:26 +0800

    1501

--- test-bilibili/src/main/java/org/xzc/bilibili/CommentService.java
+++ test-bilibili/src/main/java/org/xzc/bilibili/CommentService.java
@@ -11,7 +11,7 @@ public class CommentService {
 		if (v.mid == 1643718) //up主是山下智博
 			return true;
 		if (v.typeid == 30) {
-			return v.title.contains( "初音ミク" ) || v.title.contains( "洛天依" );
+			return v.title.contains( "初音" ) || v.title.contains( "洛天依" );
 		}
 		if (v.typeid == 71 && v.mid == 883968) //暴走漫画
 			return true;
@@ -29,7 +29,7 @@ public class CommentService {
 			return "喂, 110, 这里有大绅(变)士(态), 请速速前来!";
 		}
 		if (v.typeid == 30) {
-			if (v.title.contains( "初音ミク" )) //126 人力VOCALOID
+			if (v.title.contains( "初音" )) //126 人力VOCALOID
 				return "公主殿下的评论由我来攻占, up主加油.";
 			if (v.title.contains( "洛天依" )) //126 人力VOCALOID
 				return "天依的评论由我来攻占, up主加油.";
