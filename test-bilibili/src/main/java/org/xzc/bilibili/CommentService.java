7e704b044787b67dc765326fface0f0f9d482acb
xzchaoo <70862045@qq.com>
2015-12-09 08:39:17 +0800

    0839

--- test-bilibili/src/main/java/org/xzc/bilibili/CommentService.java
+++ test-bilibili/src/main/java/org/xzc/bilibili/CommentService.java
@@ -12,6 +12,9 @@ public class CommentService {
 	public boolean accept(Video v) {
 		if (v.mid == 1643718) //up主是山下智博
 			return true;
+		if (v.typeid == 126) {
+			return v.title.contains( "初音ミク" ) || v.title.contains( "洛天依" ); //126 人力VOCALOID
+		}
 		return v.typeid == 33/* || v.typeid == 31 || v.typeid == 20*/ || v.typeid == 32/* || v.typeid == 15*/;
 	}
 
@@ -26,6 +29,12 @@ public class CommentService {
 	public String getComment(Video v) {
 		if (v.mid == 1643718) {
 			return "喂, 110, 这里有大绅(变)士(态), 请速速前来!";
+		}
+		if (v.typeid == 126) {
+			if (v.title.contains( "初音ミク" )) //126 人力VOCALOID
+				return "公主殿下的评论由我来攻占, up主加油.";
+			if (v.title.contains( "洛天依" )) //126 人力VOCALOID
+				return "天依的评论由我来攻占, up主加油.";
 		}
 		String msg = 预定的文本.get( v.aid );
 		if (msg != null)
